856533bb104b3196514adb20912c925da3a3d522
André Gustavo de Oliveira dos Santos <58216478+andresantoss@users.noreply.github.com>
2022-03-17 09:25:06 -0300

    square movement

--- backend/SkullWarrior.py
+++ backend/SkullWarrior.py
@@ -4,15 +4,48 @@ from sys import exit
 
 pygame.init()
 
-largura = 1820
-altura = 980
+largura = 640
+altura = 480
+
+x = largura/2
+y = altura/2
 
 tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('SkullWarrior')
+relogio = pygame.time.Clock()
+
 
 while True:
+    relogio.tick(30)
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
+        '''
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20'''
+    if pygame.key.get_pressed()[K_a]:
+        x=x-20
+    if pygame.key.get_pressed()[K_d]:
+        x=x+20
+    if pygame.key.get_pressed()[K_w]:
+        y=y-20
+    if pygame.key.get_pressed()[K_s]:
+        y=y+20
+    pygame.draw.rect(tela, (255,0,0), (x, y, 40,50))
+    # if y >= altura:
+    #     y = 0
+    # y = y + 1
+    #x = x + 1
+    #pygame.draw.circle(tela, (0, 0, 255), (300,260), 40)
+    #pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5 )
+
     pygame.display.update()
